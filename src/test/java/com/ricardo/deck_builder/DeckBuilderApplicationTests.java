f706b6abc78d72a1e60a19684f629543b117595f
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-06-01 22:25:40 -0300

    initial commit

---
+++ src/test/java/com/ricardo/deck_builder/DeckBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.deck_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
